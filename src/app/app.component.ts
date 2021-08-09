52da065cbb211ec32b192a6e1939a3daf6a28eec
Alex <alex-007.88@mail.ru>
2021-08-09 23:05:38 +0300

    наннан

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
  constructor() {
- } img = 'https://sun9-73.userapi.com/impg/a-rhD2vgLB97ZU0L6TD9FsIH8Sr6GPCZjPw5bQ/0Ori5LaT4TY.jpg?size=746x746&quality=96&sign=4840005da99d1b7dce2b96768354473c&type=album'
+ }
+
+ img = 'https://sun9-73.userapi.com/impg/a-rhD2vgLB97ZU0L6TD9FsIH8Sr6GPCZjPw5bQ/0Ori5LaT4TY.jpg?size=746x746&quality=96&sign=4840005da99d1b7dce2b96768354473c&type=album'
 }
+
