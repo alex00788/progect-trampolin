b06e688b673797b5afe7db665198e96c95cdd618
Alex <alex-007.88@mail.ru>
2021-08-09 11:43:10 +0300

    зделал страницу для кажного поста

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Post, PostsService} from '../../posts.service';
+import {switchMap} from 'rxjs/operators';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-page',
-  templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+    selector: 'app-edit-page',
+    templateUrl: './edit-page.component.html',
+    styleUrls: ['./edit-page.component.scss']
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup
 
-  ngOnInit() {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private postService: PostsService
+    ) {
+    }
 
+    ngOnInit() {
+        this.route.params
+            .pipe(switchMap((params: Params) => {
+                    return this.postService.getById(params ['id']);
+                })
+            ).subscribe((post: Post) => {
+            this.form = new FormGroup({
+                title: new FormControl(post.title, Validators.required),
+                text: new FormControl(post.text, Validators.required)
+            });
+        });
+
+    }
+
+    submit() {
+
+    }
 }
