c0e792405a88552c95e6833e7cac40d6c8d0a593
Alex <alex-007.88@mail.ru>
2021-08-11 11:42:18 +0300

    в сервисе прописал методы success, warning, danger

--- src/app/admin/shared/services/alert.service.ts
+++ src/app/admin/shared/services/alert.service.ts
@@ -3,12 +3,24 @@ import {Subject} from 'rxjs';
 
 export type AlertType = 'success' | 'warning' | 'danger'
 
-export  interface Alert {
-    type: AlertType
-    text: string
+export interface Alert {
+    type: AlertType;
+    text: string;
 }
 
 @Injectable()
 export class AlertService {
-    public alert$ = new Subject<Alert>()
+    public alert$ = new Subject<Alert>();
+
+    success(text: string) {
+        this.alert$.next({type: 'success', text})
+    }
+
+    warning(text: string) {
+        this.alert$.next({type: 'warning', text})
+    }
+
+    danger(text: string) {
+        this.alert$.next({type: 'danger', text})
+    }
 }
